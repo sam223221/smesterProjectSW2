82876d33e3840660ba3daa1d11f1bcd5fdad1e3b
kamils555 <ksuchodolski@vp.pl>
2024-03-21 19:50:08 +0100

    Version 1.0.2

--- Danfoss Heating system/obj/Release/net8.0/Danfoss Heating system.AssemblyInfo.cs
+++ Danfoss Heating system/obj/Release/net8.0/Danfoss Heating system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Danfoss Heating system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7402bd91cf536189b2a8552481aa5f04dc4d05c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+589072bf19276a42b80f721c8734ad7dac2ee7be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Danfoss Heating system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Danfoss Heating system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
