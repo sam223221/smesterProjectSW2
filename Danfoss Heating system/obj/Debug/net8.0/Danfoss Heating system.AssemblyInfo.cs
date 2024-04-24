7cee8d950129afe133017ee22c8a1da35e6ebb17
sam223221 <58514851+sam223221@users.noreply.github.com>
2024-04-24 20:57:56 +0200

    x

--- Danfoss Heating system/obj/Debug/net8.0/Danfoss Heating system.AssemblyInfo.cs
+++ Danfoss Heating system/obj/Debug/net8.0/Danfoss Heating system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Danfoss Heating system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d4f87128207c23e4b528774168848fd889212a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2178d69814f977475528a3fa53ad48a149ef1d8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Danfoss Heating system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Danfoss Heating system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
